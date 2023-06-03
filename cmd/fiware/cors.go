9ff363ae83f45e1e85b9cdc119c70e02301e825c
private <private@email.com>
2023-06-03 15:36:22 +0000

    added PATCH method to CORS

--- cmd/fiware/cors.go
+++ cmd/fiware/cors.go
@@ -6,7 +6,7 @@ func cors(handle http.Handler) http.Handler {
 	wrapper := func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Origin") != "" {
 			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Max-Age", "86400")
