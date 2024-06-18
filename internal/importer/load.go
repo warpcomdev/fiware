4f22fb27144d3dda4cb9599f6981e677302c82ba
rg2011 <52279456+rg2011@users.noreply.github.com>
2024-06-18 17:44:42 +0200

    added support for ngsi files

--- internal/importer/load.go
+++ internal/importer/load.go
@@ -38,13 +38,19 @@ func Load(datafile string, params map[string]string, libPath string) (fiware.Man
 	decoder := json.NewDecoder(strings.NewReader(jsonStr))
 	decoder.DisallowUnknownFields()
 	if err := decoder.Decode(&manifest); err != nil {
-		// If that fails, decode as deployer config
-		rawDecoder := json.NewDecoder(strings.NewReader(jsonStr))
-		rawConfig := deployerConfig{}
-		if rawErr := rawDecoder.Decode(&rawConfig); rawErr != nil {
-			return manifest, fmt.Errorf("failed to unmarshal file %s: %w, then %w", datafile, err, rawErr)
+		// If that fails, decode as NGSI entity
+		ngsiManifest, ngsiErr := decode_ngsi(jsonStr)
+		if ngsiErr == nil {
+			manifest = ngsiManifest
+		} else {
+			// Finally, try to decode as deployer config
+			rawDecoder := json.NewDecoder(strings.NewReader(jsonStr))
+			rawConfig := deployerConfig{}
+			if rawErr := rawDecoder.Decode(&rawConfig); rawErr != nil {
+				return manifest, fmt.Errorf("failed to unmarshal file %s: %w, then %w and finally %w", datafile, err, rawErr, ngsiErr)
+			}
+			manifest = rawConfig.ToManifest()
 		}
-		manifest = rawConfig.ToManifest()
 	}
 	// Always add notification endpoints
 	if manifest.Environment.NotificationEndpoints == nil {
@@ -52,6 +58,60 @@ func Load(datafile string, params map[string]string, libPath string) (fiware.Man
 	}
 	for key, val := range config.EndpointsFromParams(params) {
 		manifest.Environment.NotificationEndpoints[key] = val
+	}
+	return manifest, nil
+}
+
+func decode_ngsi(jsonStr string) (fiware.Manifest, error) {
+	var data map[string]json.RawMessage
+	err := json.Unmarshal([]byte(jsonStr), &data)
+	if err != nil {
+		return fiware.Manifest{}, err
+	}
+	eId, hasId := data["id"]
+	eType, hasType := data["type"]
+	if !hasId || !hasType {
+		return fiware.Manifest{}, fmt.Errorf("missing id or type in NGSI entity")
+	}
+	var idStr, typeStr string
+	if err := json.Unmarshal(eId, &idStr); err != nil {
+		return fiware.Manifest{}, fmt.Errorf("failed to decode id: %w", err)
+	}
+	if err := json.Unmarshal(eType, &typeStr); err != nil {
+		return fiware.Manifest{}, fmt.Errorf("failed to decode type: %w", err)
+	}
+	entityType := fiware.EntityType{
+		ID:    idStr,
+		Type:  typeStr,
+		Attrs: make([]fiware.Attribute, 0, len(data)),
+	}
+	entity := fiware.Entity{
+		ID:        idStr,
+		Type:      typeStr,
+		Attrs:     make(map[string]json.RawMessage),
+		MetaDatas: make(map[string]json.RawMessage),
+	}
+	for k, v := range data {
+		if k == "id" || k == "type" {
+			continue
+		}
+		var attrib map[string]json.RawMessage
+		if err := json.Unmarshal(v, &attrib); err != nil {
+			return fiware.Manifest{}, fmt.Errorf("Failed to decode attribute %s: %w", k, err)
+		}
+		var attrTypeStr string
+		if err := json.Unmarshal(attrib["type"], &attrTypeStr); err != nil {
+			return fiware.Manifest{}, fmt.Errorf("Failed to decode type of attribute %s: %w", k, err)
+		}
+		entityType.Attrs = append(entityType.Attrs, fiware.Attribute{
+			Name: k,
+			Type: attrTypeStr,
+		})
+		entity.Attrs[k] = v
+	}
+	manifest := fiware.Manifest{
+		EntityTypes: []fiware.EntityType{entityType},
+		Entities:    []fiware.Entity{entity},
 	}
 	return manifest, nil
 }
